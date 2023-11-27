565e5427502cfee430701fff9821fdf7af9cda0b
Louis Axel <louisaxel11103@gmail.com>
2023-11-27 13:35:46 +0700

    asda

--- php/users-login.php
+++ php/users-login.php
@@ -6,27 +6,28 @@
     if (!empty($userID) && !empty($password)) {
         $sql = "SELECT * FROM users WHERE userID = '$userID' && passcode = '$password'";
         if (mysqli_num_rows(mysqli_query($conn, $sql)) > 0) {
+            // echo"'$userID";
             session_start();
             $_SESSION['userID'] = $userID;
             header('Location: users-logged.php');
         } else {
             echo "              
             <script type='text/javascript'>
-            alert('Failed');
+            alert('$userID Failed1 ');
             location='login-form.php';
             </script>";
         }
     } else {
         echo "              
         <script type='text/javascript'>
-        alert('Failed');
+        alert('Failed2');
         location='login-form.php';
         </script>";
     }
     } else {
         echo "              
         <script type='text/javascript'>
-        alert('Failed');
+        alert('Failed3');
         location='login-form.php';
         </script>";
     }
