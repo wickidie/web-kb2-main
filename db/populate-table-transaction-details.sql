74f5da8525cc33a01ebcfa481ad7dc9877436eb8
Wickidie <146711038+wickidie@users.noreply.github.com>
2023-11-29 17:14:53 +0700

    add some db file & trans details

---
+++ db/populate-table-transaction-details.sql
@@ -0,0 +1,12 @@
+INSERT INTO `transaction_details`(`transaction_id`, `product_id`, `quantity`, `product_price`) 
+VALUES ('1','1','10','0.99');
+INSERT INTO `transaction_details`(`transaction_id`, `product_id`, `quantity`, `product_price`) 
+VALUES ('1','2','20','1.99');
+INSERT INTO `transaction_details`(`transaction_id`, `product_id`, `quantity`, `product_price`) 
+VALUES ('1','3','30','5.99');
+INSERT INTO `transaction_details`(`transaction_id`, `product_id`, `quantity`, `product_price`) 
+VALUES ('2','1','40','2.99');
+INSERT INTO `transaction_details`(`transaction_id`, `product_id`, `quantity`, `product_price`) 
+VALUES ('2','2','50','3.99');
+INSERT INTO `transaction_details`(`transaction_id`, `product_id`, `quantity`, `product_price`) 
+VALUES ('2','3','60','4.99');
