4812eee1f83875fb659c0c2fe861e15c9faf8cc7
Wickidie <146711038+wickidie@users.noreply.github.com>
2023-11-29 15:23:39 +0700

    change auto increment

--- db/populate-table-products.sql
+++ db/populate-table-products.sql
@@ -1,2 +1,8 @@
-INSERT INTO products (product_id, product_name, product_description, product_price, product_img, product_category) VALUES
-(1, 'prod01', 'prod01Desc', 15.5, 'img_avatar1.png', 'prod01Cat');
+INSERT INTO products (product_name, product_description, product_price, product_img, product_category) VALUES
+('prod01', 'prod01Desc', 15.5, 'img_avatar1.png', 'prod01Cat');
+INSERT INTO products (product_name, product_description, product_price, product_img, product_category) VALUES
+('prod02', 'prod02Desc', 15.5, 'img_avatar1.png', 'prod01Cat');
+INSERT INTO products (product_name, product_description, product_price, product_img, product_category) VALUES
+('prod03', 'prod03Desc', 15.5, 'img_avatar1.png', 'prod02Cat');
+INSERT INTO products (product_name, product_description, product_price, product_img, product_category) VALUES
+('prod04', 'prod04Desc', 15.5, 'img_avatar1.png', 'prod02Cat');
