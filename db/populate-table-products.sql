c7977ad21afe6696b3bcffe0a82fa5b39a3f6a23
Wickidie <146711038+wickidie@users.noreply.github.com>
2023-11-29 14:57:12 +0700

    add sql

---
+++ db/populate-table-products.sql
@@ -0,0 +1,2 @@
+INSERT INTO products (product_id, product_name, product_description, product_price, product_img, product_category) VALUES
+(1, 'prod01', 'prod01Desc', 15.5, 'img_avatar1.png', 'prod01Cat');
--- php/db-connect.inc.php
+++ php/db-connect.inc.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
-$dbname = "web";
+$password = "Admin@123";
+$dbname = "web_kb2";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
