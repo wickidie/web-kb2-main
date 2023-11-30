84736debae2bf9b22a3aa5df66bb4fa00843e53f
Wickidie <146711038+wickidie@users.noreply.github.com>
2023-11-30 10:07:27 +0700

    add js and test.php

---
+++ js/sidebar.js
@@ -0,0 +1,63 @@
+<div class="pt-3 mb-auto">
+    <div class="d-flex text-decoration-none justify-content-center align-items-center">
+    <span class="fs-4 d-sm-inline">
+        <svg id="logo-72" width="42" height="34" viewBox="0 0 53 44" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M23.2997 0L52.0461 28.6301V44H38.6311V34.1553L17.7522 13.3607L13.415 13.3607L13.415 44H0L0 0L23.2997 0ZM38.6311 15.2694V0L52.0461 0V15.2694L38.6311 15.2694Z"
+                class="ccustom" fill="currentColor"></path>
+        </svg>
+    </span>
+    </div>
+    <ul
+    class="nav nav-pills flex-column justify-content-center align-items-sm-stretch align-items-center">
+    <li class="nav-item py-2 py-sm-0">
+        <a href="#" class="nav-link">
+            <i class="bi bi-house"></i>
+            <span class="d-none fs-5 ms-2 d-sm-inline">
+                Home
+            </span>
+        </a>
+    </li>
+    <li class="nav-item py-2 py-sm-0">
+        <a href="#" class="nav-link">
+            <i class="bi bi-speedometer2"></i>
+            <span class="d-none fs-5 ms-2 d-sm-inline">
+                Dashboard
+            </span>
+        </a>
+    </li>
+    <li class="nav-item py-2 py-sm-0">
+        <a href="transactions.php" class="nav-link">
+            <i class="bi bi-table"></i>
+            <span class="d-none fs-5 ms-2 d-sm-inline">
+                Transactions
+            </span>
+        </a>
+    </li>
+    <li class="nav-item py-2 py-sm-0">
+        <a href="transaction-details.php" class="nav-link">
+            <i class="bi bi-receipt"></i>
+            <span class="d-none fs-5 ms-2 d-sm-inline">
+                Transactions Details
+            </span>
+        </a>
+    </li>
+    <li class="nav-item py-2 py-sm-0">
+        <a href="products.php" class="nav-link">
+            <i class="bi bi-grid"></i>
+            <span class="d-none fs-5 ms-2 d-sm-inline">
+                Products
+            </span>
+        </a>
+    </li>
+    <li class="nav-item py-2 py-sm-0">
+        <a href="#" class="nav-link active" aria-current="page">
+            <i class="bi bi-person-circle"></i>
+            <span class="d-none fs-5 ms-2 d-sm-inline">
+                Users
+            </span>
+        </a>
+    </li>
+    </ul>
+    </div>
