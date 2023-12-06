f4ff97e62874b7c590f6b3211f7bb770280d4064
ntnlmg <natanael.gloryant2020@gmail.com>
2023-12-06 18:51:33 +0700

    just trying templates

--- php/test.php
+++ php/test.php
@@ -1,16 +1,3 @@
-<?php
-    session_start();
-    include_once 'db-connect.inc.php';
-    $username = $_SESSION['username'];
-    if (isset($username)&& !empty($username)) {
-    } else {
-        echo "              
-        <script type='text/javascript'>
-        alert('You must login first');
-        location='login-form.php';
-        </script>";
-    }
-?>
 <!DOCTYPE html>
 <html>
 
@@ -240,17 +227,30 @@
     </header>
     <div class="l-navbar" id="nav-bar">
         <nav class="nav">
-            <div> <a href="#" class="nav_logo"> <i class="bx bx-layer nav_logo-icon"></i> <span
-                        class="nav_logo-name">BBBootstrap</span> </a>
-                <div class="nav_list"> <a href="#" class="nav_link active"> <i class="bx bx-grid-alt nav_icon"></i>
-                        <span class="nav_name">Dashboard</span> </a> <a href="#" class="nav_link"> <i
-                            class="bx bx-user nav_icon"></i> <span class="nav_name">Users</span> </a> <a href="#"
-                        class="nav_link"> <i class="bx bx-message-square-detail nav_icon"></i> <span
-                            class="nav_name">Messages</span> </a> <a href="#" class="nav_link"> <i
-                            class="bx bx-bookmark nav_icon"></i> <span class="nav_name">Bookmark</span> </a> <a href="#"
-                        class="nav_link"> <i class="bx bx-folder nav_icon"></i> <span class="nav_name">Files</span> </a>
-                    <a href="#" class="nav_link"> <i class="bx bx-bar-chart-alt-2 nav_icon"></i> <span
-                            class="nav_name">Stats</span> </a>
+            <div> <a href="#" class="nav_logo"> <i class="bx bx-layer nav_logo-icon"></i>
+                    <span class="nav_logo-name">BBBootstrap</span> </a>
+                <div class="nav_list">
+                    <a href="#" class="nav_link active">
+                        <i class="bx bx-grid-alt nav_icon"></i>
+                        <span class="nav_name">Dashboard</span>
+                    </a>
+                    <a href="test2.php" class="nav_link">
+                        <i class="bx bx-user nav_icon"></i>
+                        <span class="nav_name">Users</span>
+                    </a>
+                    <a href="test2.php" class="nav_link">
+                        <i class="bx bx-message-square-detail nav_icon"></i>
+                        <span class="nav_name">Messages</span>
+                    </a>
+                    <a href="test2.php" class="nav_link">
+                        <i class="bx bx-bookmark nav_icon"></i>
+                        <span class="nav_name">Bookmark</span> </a>
+                    <a href="test2.php" class="nav_link">
+                        <i class="bx bx-folder nav_icon"></i>
+                        <span class="nav_name">Files</span> </a>
+                    <a href="test2.php" class="nav_link">
+                        <i class="bx bx-bar-chart-alt-2 nav_icon"></i>
+                        <span class="nav_name">Stats</span> </a>
                 </div>
             </div>
             <a href="#" class="nav_link">
