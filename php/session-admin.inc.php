a72ef0232bee62a4d3abbaab01d32087f62311c4
Wickidie <146711038+wickidie@users.noreply.github.com>
2024-01-04 07:56:03 +0700

    trash

--- php/session-admin.inc.php
+++ php/session-admin.inc.php
@@ -1,13 +1,10 @@
 <?php
     session_start();
     include_once 'db-connect.inc.php';
+    include_once 'web-kb2.inc.php';
     $user_id = $_SESSION['user_id'];
     if (isset($user_id) && !empty($user_id)) {
     } else {
-        echo "              
-        <script type='text/javascript'>
-        alert('You must login first');
-        location='login-form.php';
-        </script>";
+        alertToLocation("login-form", "AYE");
     }
 ?>
