2b891a355031aff7e9e13f121a2ec3818e0adad5
Wickidie <146711038+wickidie@users.noreply.github.com>
2024-01-09 16:59:42 +0700

    mamamia

--- db/populate-table-products.sql
+++ db/populate-table-products.sql
@@ -1,12 +1,3 @@
-INSERT INTO products (product_name, product_description, product_price, product_img, product_category) VALUES
-('prod01', 'prod01Desc', 15.5, 'prod01.jpg', 'prod01Cat');
-INSERT INTO products (product_name, product_description, product_price, product_img, product_category) VALUES
-('prod02', 'prod02Desc', 15.5, 'prod02.jpg', 'prod01Cat');
-INSERT INTO products (product_name, product_description, product_price, product_img, product_category) VALUES
-('prod03', 'prod03Desc', 15.5, 'prod03.jpg', 'prod02Cat');
-INSERT INTO products (product_name, product_description, product_price, product_img, product_category) VALUES
-('prod04', 'prod04Desc', 15.5, 'prod04.jpg', 'prod02Cat');
-
 -- Newest with category_id, add category first before adding product
 
 INSERT INTO `product_category`(`category_name`) VALUES ('Other');
