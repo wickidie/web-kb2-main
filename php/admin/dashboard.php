d72d8f2d263cde78760a178057de3657094cc47b
Louis Axel <louisaxel11103@gmail.com>
2024-01-20 18:26:11 +0700

    landingpage dll

--- php/admin/dashboard.php
+++ php/admin/dashboard.php
@@ -49,17 +49,28 @@
                     <section class="d-flex flex-column justify-content-center align-items-center">
                         <div class="container flex-grow-1 mt-3">
                             <div class="row">
+
                                 <div class="col-lg-8 mb-4 order-0">
                                     <div class="card shadow">
                                         <div class="d-flex align-items-end row">
                                             <div class="col-sm-7">
                                                 <div class="card-body">
-                                                    <h5 class="card-title text-primary">Congratulations User! 🎉</h5>
-                                                    <p class="mb-4">You have done <span class="fw-bold">72%</span> more
-                                                        sales today. Check your new badge in your profile.</p>
+                                                    <h5 class="card-title text-primary">Total Sales! 🎉</h5>
+                                                    <?php
+                                                        $sql = "SELECT sold FROM products";
+                                                        $result = mysqli_query($conn, $sql);
+                                                        $itemSold = 0;
 
-                                                    <a href="javascript:;" class="btn btn-sm btn-outline-primary">View
-                                                        Badges</a>
+                                                        if (mysqli_num_rows($result) > 0) {
+                                                            while($row = mysqli_fetch_assoc($result)) {
+                                                                $itemSold += $row['sold'];
+                                                            } 
+                                                        }
+                                                        echo "<p class='mb-4'>We Sold <span class='fw-bold'>" . $itemSold . "</span> items until now.</p>"
+                                                    ?>
+
+                                                    <!-- <a href="javascript:;" class="btn btn-sm btn-outline-primary">View
+                                                        Badges</a> -->
                                                 </div>
                                             </div>
                                             <div class="col-sm-5 text-center text-sm-left">
@@ -73,6 +84,8 @@
                                         </div>
                                     </div>
                                 </div>
+
+
                                 <div class="col-lg-4 col-md-4 order-1">
                                     <div class="row">
                                         <div class="col-lg-6 col-md-12 col-6 mb-4">
@@ -82,7 +95,7 @@
                                                         class="card-title d-flex align-items-start justify-content-between">
                                                         <div class="flex-shrink-0">
                                                             <span class="dashboard-icon dashboard-icon-success">
-                                                                <i class="bi bi-clock icon"></i>
+                                                                <i class="bi bi-person icon"></i>
                                                             </span>
                                                         </div>
                                                         <div class="dropdown">
@@ -99,13 +112,19 @@
                                                             </div>
                                                         </div>
                                                     </div>
-                                                    <span class="fw-semibold d-block mb-1">Profit</span>
-                                                    <h3 class="card-title mb-2">$12,628</h3>
-                                                    <small class="text-success fw-semibold"><i
-                                                            class="bx bx-up-arrow-alt"></i> +72.80%</small>
+                                                    <span class="fw-semibold d-block mb-1">Total Users</span>
+                                                    <?php
+                                                        $sql = "SELECT * FROM users";
+                                                        $result = mysqli_query($conn, $sql);
+                                                        $rows = mysqli_num_rows($result);
+                                                        echo "<h3 class='card-title mb-2'>" . $rows . " users</h3>";
+                                                    ?>
+                                                    <!-- <small class="text-success fw-semibold"><i
+                                                            class="bx bx-up-arrow-alt"></i> +72.80%</small> -->
                                                 </div>
                                             </div>
                                         </div>
+
                                         <div class="col-lg-6 col-md-12 col-6 mb-4">
                                             <div class="card shadow">
                                                 <div class="card-body">
@@ -130,10 +149,63 @@
                                                             </div>
                                                         </div>
                                                     </div>
-                                                    <span class="d-block mb-1">Sales</span>
-                                                    <h3 class="card-title mb-2">$12,628</h3>
-                                                    <small class="text-success fw-semibold"><i
-                                                            class="bx bx-up-arrow-alt"></i> +72.80%</small>
+                                                    <span class="d-block mb-1">Products</span>
+                                                    <?php
+                                                        $sql = "SELECT purchase FROM users";
+                                                        $result = mysqli_query($conn, $sql);
+                                                        $rows = mysqli_num_rows($result);
+                                                        echo "<h3 class='card-title mb-2'>" . $rows . " products</h3>";
+                                                    ?>
+                                                    <!-- <small class="text-success fw-semibold"><i
+                                                            class="bx bx-up-arrow-alt"></i> +72.80%</small> -->
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-lg-6 col-md-12 col-6 mb-4">
+                                            <div class="card shadow">
+                                                <div class="card-body">
+                                                    <div
+                                                        class="card-title d-flex align-items-start justify-content-between">
+                                                        <div class="avatar flex-shrink-0">
+                                                            <span class="dashboard-icon dashboard-icon-cyan">
+                                                                <i class="bi bi-wallet icon"></i>
+                                                            </span>
+                                                        </div>
+                                                        <div class="dropdown">
+                                                            <span type="button" id="cardOpt3" data-bs-toggle="dropdown"
+                                                                aria-haspopup="true" aria-expanded="false">
+                                                                <i class="bi bi-three-dots-vertical"></i>
+                                                            </span>
+                                                            <div class="dropdown-menu dropdown-menu-end"
+                                                                aria-labelledby="cardOpt3">
+                                                                <a class="dropdown-item" href="javascript:void(0);">View
+                                                                    More</a>
+                                                                <a class="dropdown-item"
+                                                                    href="javascript:void(0);">Delete</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <span class="d-block mb-1">Top Buyers</span>
+                                                    <?php
+                                                        $sql = "SELECT * FROM users";
+                                                        $result = mysqli_query($conn, $sql);
+                                                        $topBuyers = 0;
+                                                        $topBuyersName = 0;
+
+                                                        if (mysqli_num_rows($result) > 0) {
+                                                            while($row = mysqli_fetch_assoc($result)) {
+                                                                if ($topBuyers < $row['purchase']) {
+                                                                    $topBuyers = $row['purchase'];
+                                                                    $topBuyersName = $row['username'];
+                                                                }
+                                                            } 
+                                                        }
+
+                                                        echo "<h3 class='card-title mb-2'>" . $topBuyersName . "</h3>";
+                                                    ?>
+                                                    <!-- <small class="text-success fw-semibold"><i
+                                                            class="bx bx-up-arrow-alt"></i> +72.80%</small> -->
                                                 </div>
                                             </div>
                                         </div>
